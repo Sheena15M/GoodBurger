8ea9633548a75157f851469522e93be5a34eb89a
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-27 13:14:30 -0400

    more work on orm

--- config/orm.js
+++ config/orm.js
@@ -22,6 +22,21 @@ for (var key in ob) {
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
             value = "'" + value + "'";
         }
+        arr.push(key + "=" + value);
     }
 }    
+return arr.toString();
+}
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err,result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        }
+        );
+    },
 }
