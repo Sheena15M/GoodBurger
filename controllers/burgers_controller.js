e7cb6df4193ed1c09880b60b070216ceaff52a7f
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-28 01:16:50 -0400

    fixes to BC

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,10 +14,18 @@ router.get("/", function (req, res) {
 });
 
 //Burger making station
-router.post("/burger/create"), function (req, res) {
+router.post("/burger/create", function (req, res) {
     burger.create (req.body.burger_name, function (result) {
         console.log(result);
         res.redirect("");
     });
 
 });
+
+router.put ("/burger/:id", function (req, res) {
+    var condition = "id =" + req.params.id;
+    console.log ("condition", condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function (result))
+}
+)
