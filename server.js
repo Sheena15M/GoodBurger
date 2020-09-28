e0b0da90f37c8ad4b003370000ce72e9b49f7742
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-28 10:32:52 -0400

    more code to server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+//Express creates routes, no express, no routes
 var express = require ("express");
-var get propertyName() {
-    return this.;
-}
+
+//process.env.PORT is to connect to Heroku and 8080 is for local use
+var PORT = process.env.PORT || 8080;
+
+var app = express ();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
