c8e726d7f9fada6c13293bf0d7e92d3b67cfbd78
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-28 21:17:30 -0400

    more changes to orm

--- config/orm.js
+++ config/orm.js
@@ -44,11 +44,18 @@ var orm = {
     //Make your burger
     addOne: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+        
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err
+            }
+        }
+        )
     }
-    update: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-
-        queryString += " SET ";
-        queryString += objToSql (objColVals);
-    }
-};
+    
