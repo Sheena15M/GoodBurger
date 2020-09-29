d13914d036e57c79addaea2fc323e6943c3f8a79
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-29 10:31:42 -0400

    better updates to front end burger

--- Assets/Public/burger.js
+++ Assets/Public/burger.js
@@ -19,4 +19,21 @@ $(function() {
           location.reload();
       });
   });
-  
+  $(".eatburger").on("click", function(event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var devouredState = {
+        devoured: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+    }).then(function() {
+        console.log("Burger devoured");
+        location.reload();
+    });
+});
+
