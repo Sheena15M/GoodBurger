39b212b364ada52b9ee7323938a88af40ac440f5
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-09-30 23:54:25 -0400

    uppercase on seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ USE burger_db;
 
 CREATE TABLE burgers
 (
-    id int Not Null AUTO_INCREMENT,
-    burger_name varchar (255) Not Null, 
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar (255) NOT NULL, 
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
